def4feaeab7971e2b7fd85445659dd2fab1d163a
Matthias Luebken <matthias.luebken@gmail.com>
2016-03-01 17:54:55 +0100

    some formatting and more details

--- src/github.com/luebken/container-api/main.go
+++ src/github.com/luebken/container-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/fsouza/go-dockerclient"
 	"log"
@@ -26,10 +27,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Container API for: ", imageName)
-	fmt.Println("---")
-	fmt.Println("Author:  ", img.Author)
-	fmt.Println("Created: ", img.Created)
-	fmt.Println("Config: ", img.Config.Labels)
-	fmt.Println("Ports: ", img.Config.ExposedPorts)
+	fmt.Println(" -----------------------------------------------------------")
+	fmt.Println("| Image: ", imageName)
+	fmt.Println("|-----------------------------------------------------------")
+	fmt.Println("| Author:  ", img.Author)
+	fmt.Println("| Size:    ", img.Size/1000/1000, "MB")
+	fmt.Println("| Created: ", img.Created.Format("2006-01-02 15:04"))
+	fmt.Println("|-----------------------------------------------------------")
+	fmt.Println("| Container API:")
+	fmt.Println("| * Mandatory ENVs to configure:  ")
+
+	var dat []map[string]interface{}
+	availableEnvs := img.Config.Labels["com.example.available-envs"]
+
+	if err := json.Unmarshal([]byte(availableEnvs), &dat); err != nil {
+		panic(err)
+	}
+	for _, o := range dat {
+		fmt.Println("|   - ENV:          ", o["key"])
+		fmt.Println("|   - Description:  ", o["description"])
+		fmt.Println("|   - Mandatory:    ", o["mandatory"])
+	}
+	fmt.Println("| * Optional ENVs to configure:  ")
+	fmt.Println("|     - < empty >  ") //TODO
+	fmt.Println("| * Available ports:   ")
+	for key, value := range img.Config.ExposedPorts {
+		fmt.Println("|     - ", key, value)
+	}
+	fmt.Println("| * Volumes:   ")
+	for key, value := range img.Config.Volumes {
+		fmt.Println("|     - ", key, value)
+	}
+	fmt.Println(" -----------------------------------------------------------")
+	// hooks
 }
